a79a6fc81be9aa684143ae6025fba2ab40d08c36
cucka <tsvetelina.milcheva@gmail.com>
2024-01-15 13:22:55 +0200

    apply stash

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
 
-    cout<<"Hello GIT from master :) :) :) from dev New change from home"<<endl;
+    cout<<"From work Hello GIT from master :) :) :) from dev New change from home"<<endl;
 
     cout<<"Hello from DEV";
     return 0;
